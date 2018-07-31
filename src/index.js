5cfb203ea1ba927a361bb3c25552dd2c97b6a8ee
David Michaux <michaux.dr@gmail.com>
2018-07-31 15:07:02 -0400

    Export/import App

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-
+import App from './components/App'
 
 // ===============
 
 ReactDOM.render(
-	// <App />,
+	<App />,
 	document.getElementById('root')
 );
