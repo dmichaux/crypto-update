92ff185791bfbece4abb0969263759d7341dd708
David Michaux <michaux.dr@gmail.com>
2018-08-02 13:47:00 -0400

    Control SelectBar's input with local state

--- src/components/SelectBar.js
+++ src/components/SelectBar.js
@@ -5,15 +5,33 @@ class SelectBar extends React.Component {
 	// Render div of filtered currency options
 	// Send selected currencies to ContentPane's state
 
+	constructor(props) {
+		super(props);
+		this.handleChange = this.handleChange.bind(this);
+		this.state = {
+			currencyListMaster: ["BTC", "RPL", "LTC", "LLL"],
+			searchInput: '',
+		};
+	}
+
+	// handleFocus() {
+		// Fetch to API for currency list
+	// }
+
+	handleChange(event) {
+		this.setState({searchInput: event.target.value});
+	}
+
 	render () {
 		return (
 			<React.Fragment>
-				<input type="text" placeholder="Choose a currency" />
-				{/* if (this.state.value) {
-					<div>
-						grid of filtered currency options
-					</div>
-					} */}
+				<input type="text" placeholder="Choose a currency"
+								value={this.state.searchInput}
+								onChange={this.handleChange} />
+				<div>
+						masterList: {this.state.currencyListMaster}<br />
+						searchInput: {this.state.searchInput}
+				</div>
 			</React.Fragment>
 		);
 	}
