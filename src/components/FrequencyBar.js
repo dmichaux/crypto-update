814b092f7b40ac18c059bf0d751b2518b0381ffb
David Michaux <michaux.dr@gmail.com>
2018-08-02 12:50:40 -0400

    Change radio group to button tabs

--- src/components/FrequencyBar.js
+++ src/components/FrequencyBar.js
@@ -5,19 +5,33 @@ class FrequencyBar extends React.Component {
 	// Replace radio with tabs?
 	// Abstract to components?
 
+	constructor(props) {
+		super(props);
+		this.handleClick = this.handleClick.bind(this);
+	}
+
+	handleClick(event) {
+		const tabs = document.getElementsByClassName('frequency-tab');
+		
+		this.moveSelectedClass(tabs, event.target)
+		// this.props.onTabChange(event.target.value)
+	}
+
+	moveSelectedClass(elements, target) {
+		for (let element of elements) {element.classList.remove('selected-tab')}
+		target.classList.add('selected-tab')
+	}
+
 	render() {
 		return (
 			<div>
 				update every:
-				<label>
-					<input type="radio" name="frequency" value="3" />3s
-				</label>
-				<label>
-					<input type="radio" name="frequency" value="15" />15s
-				</label>
-				<label>
-					<input type="radio" name="frequency" value="30" />30s
-				</label>
+				<button type="button" value="3" className="frequency-tab"
+								onClick={this.handleClick}>3s</button>
+				<button type="button" value="15" className="frequency-tab"
+								onClick={this.handleClick}>15s</button>
+				<button type="button" value="30" className="frequency-tab"
+								onClick={this.handleClick}>30s</button>
 			</div>
 		);
 	}
