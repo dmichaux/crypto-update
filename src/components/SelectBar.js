a9f96fd3131138e1816677344752ad03c624fd8b
David Michaux <michaux.dr@gmail.com>
2018-08-03 08:33:16 -0400

    Move filter logic outside of render method

--- src/components/SelectBar.js
+++ src/components/SelectBar.js
@@ -1,46 +1,59 @@
 import React from 'react';
 
 class SelectBar extends React.Component {
-	// Input value will be controlled via local state
 	// Render div of filtered currency options
 	// Send selected currencies to ContentPane's state
 
 	constructor(props) {
 		super(props);
+		this.handleFocus = this.handleFocus.bind(this);
 		this.handleChange = this.handleChange.bind(this);
+		this.handleClick = this.handleClick.bind(this);
 		this.state = {
 			currencyListMaster: ["BTC", "RPL", "LTC", "LLL"],
 			searchInput: '',
+			searching: false,
 		};
 	}
 
-	// handleFocus() {
-		// Fetch to API for currency list master (if current state is empty)
-	// }
+	handleFocus() {
+		// TODO: Fetch to API for currency list master (if current state is empty)
+		this.setState({searching: true});
+	}
 
 	handleChange(event) {
 		this.setState({searchInput: event.target.value});
 	}
 
-	render () {
-		const masterList = this.state.currencyListMaster;
-		const searchInput = this.state.searchInput;
-		
-		const filteredList = masterList.filter( (item) => {
-			const regexp = new RegExp(searchInput, "i");
+	handleClick() {
+		this.setState({searching: false});
+	}
+
+	filterList(list, filterTerm) {
+		const filteredList = list.filter( (item) => {
+			let regexp = new RegExp(filterTerm, "i");
 			return regexp.test(item)
 		});
+		return filteredList
+	}
+
+	render () {
+		const {currencyListMaster, searchInput, searching} = this.state;
+		const filteredList = this.filterList(currencyListMaster, searchInput);
 
 		return (
 			<React.Fragment>
 				<input type="text" placeholder="Choose a currency"
 								value={searchInput}
+								onFocus={this.handleFocus}
 								onChange={this.handleChange} />
-				<div>
-						masterList: {masterList.join(' ')}<br />
-						searchInput: {searchInput}<br />
-						filteredList: {filteredList.join(' ')}
-				</div>
+				{/* TODO: abstract list div into component */}
+				{ searching && 
+					<div>
+						masterList: {currencyListMaster.join(' ')}<br />
+						filteredList: {filteredList.join(' ')}<br />
+						<button type="button" onClick={this.handleClick}>Done</button>
+				</div> }
 			</React.Fragment>
 		);
 	}
