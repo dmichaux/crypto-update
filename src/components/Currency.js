def06222f24dcd9faa428c91945f3f1118fa48c5
David Michaux <michaux.dr@gmail.com>
2018-08-14 12:56:43 -0400

    Add toFixed(6) to displayed rates

--- src/components/Currency.js
+++ src/components/Currency.js
@@ -9,13 +9,11 @@ class Currency extends React.Component {
 		const {name, id, rate, fiatExchange} = this.props;
 		const convertedRate = this.convertRate(rate);
 
-		console.log(`=== Rendering ${id} in ${fiatExchange} with rate: ${rate} ===`)
-
 		return (
 			<div>
 				<div>{id} - {name}</div>
-				<div>1 {fiatExchange} = {rate} {id}</div>
-				<div>1 {id} = {convertedRate} {fiatExchange}</div>
+				<div>1 {fiatExchange} = {rate.toFixed(6)} {id}</div>
+				<div>1 {id} = {convertedRate.toFixed(6)} {fiatExchange}</div>
 			</div>
 		);
 	}
