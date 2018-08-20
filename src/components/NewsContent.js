9e6141f44633eb8f423e3b6ada8a03cf868dd8d5
David Michaux <michaux.dr@gmail.com>
2018-08-20 16:32:28 -0400

    Add basic fetch for news by currency

--- src/components/NewsContent.js
+++ src/components/NewsContent.js
@@ -6,7 +6,8 @@ class NewsContent extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			topNews: []
+			topNews: [],
+			currencyNews: []
 		};
 	}
 
@@ -16,29 +17,51 @@ class NewsContent extends React.Component {
 		this.fetchTopNews()
 	}
 
+	componentDidUpdate(prevProps) {
+		const {currencies} = this.props;
+		if (currencies.length !== prevProps.currencies.length) {
+			console.log(`Calling 'if' inside NewsContent componentDidUpdate`)
+			this.fetchCurrencyNews(currencies[0].name.toLowerCase())
+		}
+	}
+
 	// ===== API Callers
 
 	fetchTopNews() {
 		NewsAPI.getTopNews()
 			.then( (articles) => this.setState({
-				topNews: articles.slice(0,3),
-			}))
+				topNews: articles.slice(0,2),
+			}));
+	}
+
+	fetchCurrencyNews(currency) {
+		console.log(`===== Fetching currencyNews`)
+		NewsAPI.getTopNewsByCoin(currency)
+			.then( (articles) => this.setState({
+				currencyNews: articles[0]
+			}));
 	}
 
 	// ===== Render
 
 	render() {
-		const {topNews} = this.state;
+		const {topNews, currencyNews} = this.state;
+		console.log(`currencyNews: ${currencyNews}`)
 
 		return (
-			<div>Top News
-			{topNews.length &&
-				<ol>
-					<li><a href={topNews[0].url}>{topNews[0].title}</a></li>
-					<li><a href={topNews[1].url}>{topNews[1].title}</a></li>
-					<li><a href={topNews[2].url}>{topNews[2].title}</a></li>
-				</ol>}
-			</div>
+			<React.Fragment>
+				<div>Top News
+				{topNews.length &&
+					<ol>
+						<li><a href={topNews[0].url}>{topNews[0].title}</a></li>
+						<li><a href={topNews[1].url}>{topNews[1].title}</a></li>
+					</ol>}
+				</div>
+				<div>
+					Top News for currency:<br />
+					{currencyNews && <a href={currencyNews.url}>{currencyNews.title}</a>}
+				</div>
+			</React.Fragment>
 		);
 	}
 }
