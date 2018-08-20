64641430e08909ea1f7aae9d946c9984e36180a7
David Michaux <michaux.dr@gmail.com>
2018-08-20 14:42:35 -0400

    Add basic API fetch for top news, lifecycle hook, and state

--- src/components/NewsContent.js
+++ src/components/NewsContent.js
@@ -3,8 +3,43 @@ import React from 'react';
 import NewsAPI from '../SDKs/CryptoNewsAPI';
 
 class NewsContent extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			topNews: []
+		};
+	}
+
+	// ===== Lifecyle
+
+	componentDidMount() {
+		this.fetchTopNews()
+	}
+
+	// ===== API Callers
+
+	fetchTopNews() {
+		NewsAPI.getTopNews()
+			.then( (articles) => this.setState({
+				topNews: articles.slice(0,3),
+			}))
+	}
+
+	// ===== Render
+
 	render() {
-		return ();
+		const {topNews} = this.state;
+
+		return (
+			<div>Top News
+			{topNews.length &&
+				<ol>
+					<li><a href={topNews[0].url}>{topNews[0].title}</a></li>
+					<li><a href={topNews[1].url}>{topNews[1].title}</a></li>
+					<li><a href={topNews[2].url}>{topNews[2].title}</a></li>
+				</ol>}
+			</div>
+		);
 	}
 }
 
