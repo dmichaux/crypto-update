457c30aff9152f9576bd2c13ed195964be3b3dca
David Michaux <michaux.dr@gmail.com>
2018-08-21 15:02:02 -0400

    Export basic CurrencyNews component

---
+++ src/components/CurrencyNews.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const CurrencyNews = (props) => {
+	const {name} = props;
+	let {news} = props;
+
+	let element;
+	switch(news) {
+		case null:
+			element = <p>Loading...</p>;
+			break;
+		case news.url === null:
+			element = <p>{news.title}</p>;
+			break;
+		default:
+			element = <a href={news.url}>{news.title}</a>;
+	}
+
+	return (
+		<div>
+			Top News for {name}<br />
+			{element}
+		</div>
+	);
+}
+
+export default CurrencyNews;
