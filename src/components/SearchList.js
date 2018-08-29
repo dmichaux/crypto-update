f5b7c148a1a7b623046f08618e3ed787e8a6e715
David Michaux <michaux.dr@gmail.com>
2018-08-29 13:10:21 -0400

    Add basic CSS grid to filtered list

--- src/components/SearchList.js
+++ src/components/SearchList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import '../stylesheets/SearchList.css';
 
 import CurrencyContext from '../contexts/CurrencyContext';
 
@@ -11,7 +12,7 @@ class SearchList extends React.Component {
 	listToButtons(list, handler, addedClass="") {
 		const buttons = list.map( (item) => {
 			return ( <button key={item.id} type="button"
-												value={JSON.stringify(item)} className={addedClass}
+												value={JSON.stringify(item)} className={"grid-item" + addedClass}
 												onClick={handler}>{item.name}</button> )
 		});
 		return buttons
@@ -26,7 +27,7 @@ class SearchList extends React.Component {
 		const maxedOut = selectedNum >= 4 ?
 			<span>max currencies selected - to change currencies, remove a selection</span>
 			: false
-		const topSixteen = filteredList.slice(0, 16);
+		const topTwelve = filteredList.slice(0, 12);
 
 		return (
 				<CurrencyContext.Consumer>
@@ -36,10 +37,11 @@ class SearchList extends React.Component {
 
 						return (
 							<React.Fragment>
-								<div>Selected {selectedNum} of 4: {this.listToButtons(selected, handler, 'selected-currency')}</div>
-								<div>
-									<button type="button" onClick={handleDoneClick}>Done</button>
-									{filteredNum} Currencies: {loading || maxedOut || this.listToButtons(topSixteen, handler)}
+								<div>Selected {selectedNum} of 4: {this.listToButtons(selected, handler, ' selected-currency')}</div>
+								<div className="grid-container">
+									<span className="grid-item-num">{filteredNum} Currencies</span>
+									<button type="button" className="grid-item-done" onClick={handleDoneClick}>x</button>
+									{loading || maxedOut || this.listToButtons(topTwelve, handler)}
 								</div>
 							</React.Fragment>
 						)}}
