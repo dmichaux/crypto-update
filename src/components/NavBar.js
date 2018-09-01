d17b1601e226a5ea04250f7c8ba7cfac23bb747e
David Michaux <michaux.dr@gmail.com>
2018-09-01 14:40:26 -0400

    Add/style <hr>s to nav grid

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -28,12 +28,14 @@ class NavBar extends React.Component {
 	render () {
 		return (
 			<nav className="grid-nav">
+				<hr className="hr-left" />
 				<button type="button" value="1" className="nav-tab value-tab selected-tab"
 								onClick={this.handleClick}>Value</button>
 				<button type="button" value="2" className="nav-tab news-tab"
 								onClick={this.handleClick}>News</button>
 				<button type="button" value="3" className="nav-tab about-tab"
 								onClick={this.handleClick}>About</button>
+				<hr className="hr-right" />
 			</nav>
 		);
 	}
