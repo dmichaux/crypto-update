c4574250e0ad524251c7aa227a0ac1a95304d3f5
David Michaux <michaux.dr@gmail.com>
2018-09-01 15:38:49 -0400

    Add function to remove focus outline unless user is tabbing

--- src/index.js
+++ src/index.js
@@ -10,3 +10,13 @@ ReactDOM.render(
 	<App />,
 	document.getElementById('root')
 );
+
+// Adds focus back to tab-able elements for Accessibility concerns
+function handleFirstTab(e) {
+	if (e.keyCode === 9) {
+		document.body.classList.add('accessible-tabbing');
+		window.removeEventListener('keydown', handleFirstTab);
+	}
+}
+
+window.addEventListener('keydown', handleFirstTab);
